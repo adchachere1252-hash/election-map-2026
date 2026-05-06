9be8eee51d96472253c0c5ee870f3d4c8d4340a2
adchachere1252-hash <278534930+adchachere1252-hash@users.noreply.github.com>
2026-05-05 23:59:00 -0400

    Redeploy trigger: Wed May  6 03:59:00 UTC 2026 - fix cron cookie auth

--- server/scheduledApUpdate.ts
+++ server/scheduledApUpdate.ts
@@ -398,3 +398,4 @@ export async function handleScheduledApUpdate(req: Request, res: Response): Prom
 // Redeploy trigger: Wed May  6 03:27:42 UTC 2026 - scheduled route must be active
 // Redeploy trigger: Wed May  6 03:30:00 UTC 2026
 // Rebuild trigger: Wed May  6 03:53:41 UTC 2026
+// Redeploy trigger: Wed May  6 03:59:00 UTC 2026 - fix cron cookie auth
